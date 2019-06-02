2470259711836cc31b4d29c7b52b60cc5347b1f1
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-06-02 20:47:41 +0100

    adding post and using postman to test api

--- RESTful/api-building/src/main/java/io/java/springbootstart/topic/TopicController.java
+++ RESTful/api-building/src/main/java/io/java/springbootstart/topic/TopicController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 //marks class as controller
@@ -15,7 +17,7 @@ public class TopicController {
 	private TopicService topicService;
 	
 	
-	//URL mapping
+	//URL mapping get request
 	@RequestMapping("/topics")
 	// Response gets auto converted to JSON as a HTTP request
 	//return list of topic objects
@@ -33,6 +35,12 @@ public class TopicController {
 		return topicService.getTopic(id);
 	}
 	
-	
+	//map  post request to /topics
+	@RequestMapping(method = RequestMethod.POST , value ="/topics")
+	//send object 
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+		
+	}
 	
 }
--- RESTful/api-building/src/main/java/io/java/springbootstart/topic/TopicService.java
+++ RESTful/api-building/src/main/java/io/java/springbootstart/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.java.springbootstart.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,12 +10,14 @@ import org.springframework.stereotype.Service;
 public class TopicService {
 
 	//this is now a service 
-	private List<Topic> topics = Arrays.asList(
+	//creates an immutable list of topics
+	//create a new ArrayList to change that and make it mutable
+	private List<Topic> topics = new ArrayList<> (Arrays.asList(
 			//these onjects will be converted to JSON automatically 
 			new Topic("spring" , "framework" , "framework description"),
 			new Topic("java", "JEE" , "core description"),
 			new Topic("typescript","typescript"," typescript description")
-			);
+			));
 	
 	public List<Topic> getAllTopics(){
 		return topics;
@@ -24,5 +27,9 @@ public class TopicService {
 		//uses lambda to return topic id
 		//compare id to id that is passed in and get the first one
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 }
