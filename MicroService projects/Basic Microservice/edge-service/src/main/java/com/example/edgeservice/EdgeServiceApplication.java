83bef5df6797c65f3ffb5124aa497f2e7e3004b4
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-09-24 20:26:58 +0100

    adding dep

--- MicroService projects/Basic Microservice/edge-service/src/main/java/com/example/edgeservice/EdgeServiceApplication.java
+++ MicroService projects/Basic Microservice/edge-service/src/main/java/com/example/edgeservice/EdgeServiceApplication.java
@@ -1,13 +1,69 @@
 package com.example.edgeservice;
 
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.Resources;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import lombok.Data;
+
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableZuulProxy
 @SpringBootApplication
 public class EdgeServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EdgeServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeServiceApplication.class, args);
+    }
+}
 
+@Data
+class Item {
+    private String name;
+
+	public String getName() {
+		return name;
+	}
+}
+
+@FeignClient("item-catalog-service")
+interface ItemClient {
+
+    @GetMapping("/items")
+    Resources<Item> readItems();
+}
+
+@RestController
+class GoodItemApiAdapterRestController {
+
+    private final ItemClient itemClient;
+
+    public GoodItemApiAdapterRestController(ItemClient itemClient) {
+        this.itemClient = itemClient;
+    }
+
+    @GetMapping("/good-item")
+    public Collection<Item> gooditems() {
+        return itemClient.readItems()
+                .getContent()
+                .stream()
+                .filter(this::isGreat)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isGreat(Item item) {
+        return !item.getName().equals("name 1");
+    }
 }
