cabe8f0476aaebe3e9a4cdf50e1cdfcc57d5f08f
maksthorn <maksdrzezdzon@gmail.com>
2019-10-21 14:34:13 +0100

    java practice

---
+++ Hackerrank/src/SockMerchant.java
@@ -0,0 +1,64 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class SockMerchant {
+    /*
+    https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+     */
+
+
+
+    public static class Solution {
+
+        // Complete the sockMerchant function below.
+        static int sockMerchant(int n, int[] ar) {
+            ArrayList<Integer> pair = new ArrayList<Integer>();
+            HashSet<Integer> unique = new HashSet<Integer>();
+
+            for (int sock:ar){
+                if (unique.contains(sock)){
+                    unique.remove(sock);
+                    pair.add(sock);
+                }else{
+                    unique.add(sock);
+                }
+            }
+
+            return pair.size();
+        }
+
+        private static final Scanner scanner = new Scanner(System.in);
+
+        public static void main(String[] args) throws IOException {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] ar = new int[n];
+
+            String[] arItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int arItem = Integer.parseInt(arItems[i]);
+                ar[i] = arItem;
+            }
+
+            int result = sockMerchant(n, ar);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+
+            bufferedWriter.close();
+
+            scanner.close();
+        }
+    }
+
+
+}
