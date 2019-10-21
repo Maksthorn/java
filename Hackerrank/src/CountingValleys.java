e69fab28349ad73854c36b2805cb8c99d97df91f
maksthorn <maksdrzezdzon@gmail.com>
2019-10-21 14:57:33 +0100

    java

--- Hackerrank/src/CountingValleys.java
+++ Hackerrank/src/CountingValleys.java
@@ -8,9 +8,27 @@ public class CountingValleys {
 
         // Complete the countingValleys function below.
         static int countingValleys(int n, String s) {
+            int altitude=0;
+            int num_valley=0;
 
-            return 1;
+            for(int i=0; i<n; i++){
+                if(s.charAt(i)=='U'){
+                    if(altitude== -1) {
+                        num_valley++;
+                    }
+                    altitude++;
+                }
+
+                if (s.charAt(i) == 'D') {
+                    altitude--;
+                }
+            }
+
+            return num_valley;
         }
+
+
+    }
 
         private static final Scanner scanner = new Scanner(System.in);
 
@@ -22,7 +40,7 @@ public class CountingValleys {
 
             String s = scanner.nextLine();
 
-            int result = countingValleys(n, s);
+            int result = Solution.countingValleys(n, s);
 
             bufferedWriter.write(String.valueOf(result));
             bufferedWriter.newLine();
