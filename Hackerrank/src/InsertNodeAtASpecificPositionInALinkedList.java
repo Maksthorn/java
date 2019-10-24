522e4f1a96c1b8b44dafdcaa5b8034d944d1a16a
maksthorn <maksdrzezdzon@gmail.com>
2019-10-24 18:21:31 +0100

    more interview stuff

--- Hackerrank/src/InsertNodeAtASpecificPositionInALinkedList.java
+++ Hackerrank/src/InsertNodeAtASpecificPositionInALinkedList.java
@@ -50,17 +50,7 @@ public class InsertNodeAtASpecificPositionInALinkedList {
             }
         }
 
-        // Complete the insertNodeAtPosition function below.
 
-        /*
-         * For your reference:
-         *
-         * SinglyLinkedListNode {
-         *     int data;
-         *     SinglyLinkedListNode next;
-         * }
-         *
-         */
         static SinglyLinkedListNode insertNodeAtPosition
         (SinglyLinkedListNode head, int data, int position) {
 
