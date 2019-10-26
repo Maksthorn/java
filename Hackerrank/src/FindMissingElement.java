0bc9208eba08db3b173acd8e5450e7e5135c6553
maksthorn <maksdrzezdzon@gmail.com>
2019-10-26 10:54:19 +0100

    interview prep

---
+++ Hackerrank/src/FindMissingElement.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class FindMissingElement {
+
+    class Solution {
+        public int solution(int[] A) {
+            Arrays.sort(A);
+            int min = 1;
+            int len = A.length;
+
+            for (int i = 0; i < len; i++){
+                if(A[i] == min){
+                    min++;
+                }
+            }
+            return min;
+        }
+    }
+
+}
