c4812522db96c8c09a0ff511e0cdeef9fb987434
maksthorn <maksdrzezdzon@gmail.com>
2019-12-03 19:20:18 +0000

    some practice on java over python

---
+++ Hackerrank/src/MergeTwoLinkedLists.java
@@ -0,0 +1,40 @@
+public class MergeTwoLinkedLists {
+
+    public ListNode merge(ListNode head1, ListNode head2){
+        ListNode ptr = new ListNode(0);
+        ListNode head = ptr;
+
+        if (head1 == null){
+            return head2;
+        }
+        if (head2 == null){
+            return head1;
+        }
+
+        while(head1 != null && head2 != null){
+            if (head1.val <= head2.val){
+                ptr.next = head1;
+                head1 = head1.next;
+            }else {
+                ptr.next =head2;
+                head2 = head2.next;
+            }
+            ptr = ptr.next;
+        }
+
+        if(head1 != null){
+            ptr.next = head1;
+        }
+
+        if (head2 != null){
+            ptr.next = head2;
+        }
+
+        return head.next;
+    }
+}
+  class ListNode {
+      int val;
+      ListNode next;
+    ListNode(int x) { val = x; }
+  }
