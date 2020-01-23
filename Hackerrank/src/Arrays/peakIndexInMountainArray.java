57b6c44dc252ab9cb79ac9eeacf71acdd59fc94b
maksthorn <maksdrzezdzon@gmail.com>
2020-01-23 16:54:41 +0000

    interview prep

--- Hackerrank/src/Arrays/peakIndexInMountainArray.java
+++ Hackerrank/src/Arrays/peakIndexInMountainArray.java
@@ -1,6 +1,19 @@
 package Arrays;
 
 public class peakIndexInMountainArray {
+
+    public int peakIndexInMountainArray(int[] A) {
+        int tmp = 0;
+        for(int i = 0; i < A.length; i++){
+            tmp = A[i];
+
+            if (tmp > A[i + 1] && tmp > A[i -1]){
+                return tmp;
+            }
+        }
+        return tmp;
+    }
+
     //https://leetcode.com/submissions/detail/288477780/
     //TODO look up binary search again
 }
