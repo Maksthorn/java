cf5da092bef431c6caa95664d0df1864e7c73a3b
maksthorn <maksdrzezdzon@gmail.com>
2020-02-23 16:09:07 +0000

    Pque quesstion

---
+++ Hackerrank/src/Strings/MeetingRooms.java
@@ -0,0 +1,29 @@
+package Strings;
+
+import java.util.PriorityQueue;
+import java.util.*;
+
+class Interval {
+    int start;
+    int end;
+    Interval(){start = 0; end = 0;}
+    Interval(int s, int e){start = s; end = e;}
+}
+
+public class MeetingRooms {
+    public int minMeetingRooms(Interval[] intervals){
+        if (intervals == null || intervals.length == 0) return 0;
+        Arrays.sort(intervals, (i0, i1) -> (i0.start - i1.start));
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+        for(Interval i : intervals){
+            if (heap.size() > 0 && heap.peek() <= i.start){
+                heap.poll();
+            }
+            
+            heap.offer(i.end);
+        }
+        return heap.size();
+    }
+}
