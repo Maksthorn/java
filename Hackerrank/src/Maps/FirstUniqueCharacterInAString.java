133608e0be5c8e40153ba41182d73004baaec369
maksthorn <maksdrzezdzon@gmail.com>
2020-03-26 23:51:11 +0000

    .

--- Hackerrank/src/Maps/FirstUniqueCharacterInAString.java
+++ Hackerrank/src/Maps/FirstUniqueCharacterInAString.java
@@ -8,12 +8,14 @@ public class FirstUniqueCharacterInAString {
     // .map(Tests::doubleIt)
     // is equivalent to
     // .map(i -> Tests.doubleIt(i))
-    public int firstUniqChar(String s) {
+    public static int firstUniqChar(String s) {
         HashMap<Character, Integer> counts = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
+            System.out.println("char is " + ch);
             counts.merge(ch, 1, Integer::sum);
+            System.out.println(counts);
         }
 
         for (int i = 0; i < s.length(); i++) {
@@ -24,4 +26,12 @@ public class FirstUniqueCharacterInAString {
         }
         return -1;
     }
+
+
+    public static void main(String[] args) {
+        String a = "test";
+        System.out.println(firstUniqChar(a));
+
+    }
+
 }
